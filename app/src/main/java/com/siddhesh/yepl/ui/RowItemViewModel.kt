18d2d7ca980fa926f586ab592f881d1f8be0d4c7
siddhesh_chandorkar <siddhesh.chandorkar@paynearby.in>
2022-03-30 23:45:39 +0530

    - displayed : address, phone, price,distance

--- app/src/main/java/com/siddhesh/yepl/ui/RowItemViewModel.kt
+++ app/src/main/java/com/siddhesh/yepl/ui/RowItemViewModel.kt
@@ -1,5 +1,6 @@
 package com.siddhesh.yepl.ui
 
+import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.siddhesh.yepl.models.SearchBusinessModel
@@ -9,6 +10,25 @@ class RowItemViewModel(searchBusinessModel: SearchBusinessModel) : ViewModel() {
     var title = MutableLiveData<String>(searchBusinessModel.name)
     var transactions = MutableLiveData<String>()
     var rating = MutableLiveData<Float>(searchBusinessModel.rating)
+    var reviewCount = MutableLiveData<String>(searchBusinessModel.reviewCount.toString())
+    var address = MutableLiveData<String>(
+        searchBusinessModel.location!!.displayAddress.toString().replace("[", "").replace("]", "")
+    )
+    var phone = MutableLiveData<String>(searchBusinessModel.phone)
+    var price = MutableLiveData<String>().apply {
+        value = if (!TextUtils.isEmpty(searchBusinessModel.price)) {
+            "Price : ${searchBusinessModel.price}"
+        } else {
+            ""
+        }
+    }
+    var distance = MutableLiveData<String>().apply {
+        value = if (searchBusinessModel.distance!! <= 1000) {
+            String.format("%.2f M", searchBusinessModel.distance)
+        } else {
+            String.format("%.2f KM", searchBusinessModel.distance / 1000)
+        }
+    }
 
 
 }
